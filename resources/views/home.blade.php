cdd374dffa9cc6e93a1a598fd64d6366adf3424c
Tuyen Dang <tuyendq@gmail.com>
2023-09-30 00:07:57 +0700

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Hello Lavarel view! {{ $name }}</h1>   
+@if ( $name !== 'Tuyen' )
+<h1>You're admin!</h1>  
+@else
+<h1>{{$name}}, you're nobody!</h1>   
+@endif 
+</body>
+</html>
